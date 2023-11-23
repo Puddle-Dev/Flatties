adb8f41b8c00b753b363a2181e6a16c474920ce2
wsking233 <wsking233@gmail.com>
2023-11-23 22:08:38 +1300

    add MUI independency

--- flatties-frontend/src/App.tsx
+++ flatties-frontend/src/App.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/home/HomePage';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import AlertTitle from '@mui/material/AlertTitle';
+import Container from '@mui/material/Container';
+
 
 function App() {
   return (
-    <div className="App">
+    <Container maxWidth="xl">
+      {/* example of using material ui */}
+      <Stack sx={{ width: '100%' }} spacing={2}>
+        <Alert severity="success">
+        <AlertTitle>Success</AlertTitle>
+        Material UI uses Emotion as its default styling engine! — <strong>Supported!</strong>
+      </Alert>
+      </Stack>
+      
+      {/* import the page components */}
       <HomePage />
-    </div>
+
+    </Container>
   );
 }
 
--- flatties-frontend/src/pages/home/HomePage.tsx
+++ flatties-frontend/src/pages/home/HomePage.tsx
@@ -5,6 +5,7 @@ function HomePage() {
     <div>
       <p>Hello, world!</p>
       <p>Home page</p>
+
     </div>
   );
 }
