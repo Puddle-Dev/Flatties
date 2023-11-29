2b69bc90dd460694b0fbd170df359a9fa9041e56
Benjamin501 <144312142+Bnjmn-P@users.noreply.github.com>
2023-11-29 18:54:25 +1300

    Update ProfilePage.tsx
    Added an editable bio section

--- flatties-frontend/src/pages/profile/ProfilePage.tsx
+++ flatties-frontend/src/pages/profile/ProfilePage.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../components/layout/navBar/NavBar";
 
 function ProfilePage() {
+  const [editable, setEditable] = useState(false); // State to manage edit mode
+  const [text, setText] = useState("Add a Bio"); // State to hold the text input value
+
+  const handleEditToggle = () => {
+    setEditable(!editable); // Toggle the editable state
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value); // Update the state with the input value
+  };
+
   return (
     <div>
       <h1>Profile Page</h1>
+      {editable ? ( // Conditional rendering based on editable state
+        <div>
+          <input
+            type="text"
+            value={text}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleEditToggle}>Save</button>
+        </div>
+      ) : (
+        <div>
+          <p>{text}</p>
+          <button onClick={handleEditToggle}>Edit</button>
+        </div>
+      )}
     </div>
   );
 }
