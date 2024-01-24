feb3512b0dac1e2c07033a72027c096cde8c5809
Shuai Wang <wsking233@gmail.com>
2024-01-24 20:38:49 +1300

    change the layout of footer

--- flatties-frontend/src/components/layout/footer/Footer.tsx
+++ flatties-frontend/src/components/layout/footer/Footer.tsx
@@ -1,45 +1,33 @@
 import React from "react";
-import { Typography, List, ListItem, Link, Tab } from "@mui/material";
+import { Typography, Link, Tab, IconButton } from "@mui/material";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
   return (
     <div>
       <div>
-        <Typography variant="h5">Connect With Us</Typography>
-        <List>
-          <ListItem>
-            <strong>Eirik Enriquez : </strong>
-            <Link
-              href="https://www.linkedin.com/in/eirik-mykel-navarro-enriquez/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Tab />LinkedIn
-
-            </Link>
-          </ListItem>
-          <ListItem>
-            <strong>Benjamin Polglase : </strong>
-            <Link
+        <Typography variant="h5">Contact us</Typography>
+            
+            <IconButton
               href="https://www.linkedin.com/in/benjamin-polglase-ba3022275"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Tab />LinkedIn
-            </Link>
-          </ListItem>
-          <ListItem>
-            <strong>William Wang : </strong>
-            <Link
-              href="https://www.linkedin.com/in/william-wang-shuai/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Tab />LinkedIn
+              color="primary"
+            ><LinkedInIcon fontSize="large" /></IconButton>
+              <strong>Benjamin Polglase</strong>
+            <Tab />
 
-            </Link>
-          </ListItem>
-        </List>
+            <IconButton
+              href="https://www.linkedin.com/in/eirik-mykel-navarro-enriquez"
+              color="primary"
+            ><LinkedInIcon fontSize="large" /></IconButton>
+              <strong>Eirik Enriquez</strong>
+            <Tab />
+
+            <IconButton
+              href="https://www.linkedin.com/in/william-wang-shuai"
+              color="primary"
+            ><LinkedInIcon fontSize="large" /></IconButton>
+              <strong>William Wang</strong>
+            <Tab />
       </div>
     </div>
   );
