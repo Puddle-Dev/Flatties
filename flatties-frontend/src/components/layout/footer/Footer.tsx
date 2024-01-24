b1e46a58212c09cbea2ab177cb261a109dbe5d9a
Shuai Wang <wsking233@gmail.com>
2024-01-24 21:04:54 +1300

    change the NavBar layout

--- flatties-frontend/src/components/layout/footer/Footer.tsx
+++ flatties-frontend/src/components/layout/footer/Footer.tsx
@@ -5,30 +5,28 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 function Footer() {
   return (
     <div>
-      <div>
-        <Typography variant="h5">Contact us</Typography>
-            
-            <IconButton
-              href="https://www.linkedin.com/in/benjamin-polglase-ba3022275"
-              color="primary"
-            ><LinkedInIcon fontSize="large" /></IconButton>
-              <strong>Benjamin Polglase</strong>
-            <Tab />
+      <Typography variant="h5">Contact us</Typography>
 
-            <IconButton
-              href="https://www.linkedin.com/in/eirik-mykel-navarro-enriquez"
-              color="primary"
-            ><LinkedInIcon fontSize="large" /></IconButton>
-              <strong>Eirik Enriquez</strong>
-            <Tab />
+      <IconButton
+        href="https://www.linkedin.com/in/benjamin-polglase-ba3022275"
+        color="primary"
+      ><LinkedInIcon fontSize="large" /></IconButton>
+      <strong>Benjamin Polglase</strong>
+      <Tab />
 
-            <IconButton
-              href="https://www.linkedin.com/in/william-wang-shuai"
-              color="primary"
-            ><LinkedInIcon fontSize="large" /></IconButton>
-              <strong>William Wang</strong>
-            <Tab />
-      </div>
+      <IconButton
+        href="https://www.linkedin.com/in/eirik-mykel-navarro-enriquez"
+        color="primary"
+      ><LinkedInIcon fontSize="large" /></IconButton>
+      <strong>Eirik Enriquez</strong>
+      <Tab />
+
+      <IconButton
+        href="https://www.linkedin.com/in/william-wang-shuai"
+        color="primary"
+      ><LinkedInIcon fontSize="large" /></IconButton>
+      <strong>William Wang</strong>
+      <Tab />
     </div>
   );
 }
