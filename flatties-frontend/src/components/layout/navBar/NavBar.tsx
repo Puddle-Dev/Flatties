fe80bfd93d3134fb43f42b780ec35a1e7760d3c0
Eirik Enriquez <eirikdbbd@gmail.com>
2024-01-30 14:30:25 +1300

    updated navbar logo

--- flatties-frontend/src/components/layout/navBar/NavBar.tsx
+++ flatties-frontend/src/components/layout/navBar/NavBar.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import LOGO from "../../../assets/images/AUT-logo-block.jpg";
-import { Avatar, Stack, Paper, IconButton, ListItemIcon, Typography } from "@mui/material";
-import LoginIcon from '@mui/icons-material/Login';
+import LOGO from "../../../assets/images/flatties-long.png";
+import {
+  Avatar,
+  Stack,
+  Paper,
+  IconButton,
+  ListItemIcon,
+  Typography,
+} from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
 import { Link, NavLink } from "react-router-dom";
 import LoginModal from "../login/Login"; // Import the LoginModal component
 
@@ -19,43 +26,51 @@ function NavBar() {
   return (
     <Paper elevation={3} square={false} style={{ padding: "10px" }}>
       <Stack className="Container" direction="column">
-        <Stack className="Container-top" direction="row" borderBottom={1} justifyContent={"space-between"}>
+        <Stack
+          className="Container-top"
+          direction="row"
+          borderBottom={1}
+          justifyContent={"space-between"}
+        >
           {/* top-left part */}
           <Stack className="Container-left" direction="row">
             <img
               src={LOGO}
               alt="LOGO"
               style={{
-                width: "20%",
-                maxWidth: "400px",
+                // width: "20%",
+                maxWidth: "200px",
                 height: "auto",
                 marginRight: "10px",
               }}
             />
-            <h1>Flatties</h1>
           </Stack>
           {/* top-right part */}
-          <Stack className="Container-right" direction="row-reverse" spacing={2}>
-
+          <Stack
+            className="Container-right"
+            direction="row-reverse"
+            spacing={2}
+          >
             <IconButton
               edge="end"
               aria-label="Log In"
               size="large"
               color="inherit"
-              style={{ background: 'transparent' }}
+              style={{ background: "transparent" }}
               onClick={handleLoginClick}
             >
               <ListItemIcon>
-
                 <LoginIcon />
               </ListItemIcon>
               <Typography variant="body1" component="span">
-                <Link to="#" style={{ color: 'inherit', textDecoration: 'none' }}>
+                <Link
+                  to="#"
+                  style={{ color: "inherit", textDecoration: "none" }}
+                >
                   Log In
                 </Link>
-                </Typography>
+              </Typography>
             </IconButton>
-
           </Stack>
         </Stack>
         {/* buttom part */}
