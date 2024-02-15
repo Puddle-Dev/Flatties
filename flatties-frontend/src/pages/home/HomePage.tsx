9e96056faccdf71656f9815265bf2368cbff5069
Benjamin <144312142+Bnjmn-P@users.noreply.github.com>
2024-02-16 12:10:43 +1300

    side scrolling listings
    Created listings that scroll sideways.

--- flatties-frontend/src/pages/home/HomePage.tsx
+++ flatties-frontend/src/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
-// HomePage.tsx
 import React from "react";
-import { Typography, Button } from "@mui/material";
+import { Typography, Button, Box, List, Paper } from "@mui/material";
 import { Link } from "react-router-dom";
+import "./HomePage.css";
 
 function HomePage() {
   return (
@@ -23,6 +23,38 @@ function HomePage() {
         >
           Register Here
         </Button>
+      </div>
+      <div className="scroll-container">
+        <Box
+          sx={{
+            display: "flex",
+            overflowX: "auto",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {[1, 2, 3, 4, 5].map((index) => (
+            <List key={index} sx={{ marginRight: 2 }}>
+              <Paper sx={{ width: 300, minWidth: 250, marginBottom: "8px" }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    p: 2,
+                    fontStyle: { textAlign: "left" },
+                  }}
+                >
+                  <h3>Index: {index}</h3>
+                  <h3>Property ID: </h3>
+                  <h3>City: </h3>
+                  <h3>Property Type: </h3>
+                  <h3>BedRooms: </h3>
+                  <h3>BathRooms: </h3>
+                </Box>
+              </Paper>
+            </List>
+          ))}
+        </Box>
       </div>
     </div>
   );
