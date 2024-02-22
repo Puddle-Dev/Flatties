c341b9c02ab139cba2997e168300a2b878f26d27
Eirik Enriquez <eirikdbbd@gmail.com>
2024-02-23 12:29:15 +1300

    removed unused imports

--- flatties-frontend/src/pages/listing/ListingPage.tsx
+++ flatties-frontend/src/pages/listing/ListingPage.tsx
@@ -1,21 +1,8 @@
 import React, { useState, useEffect } from "react";
-
 import api from "../../services/api";
-
-import { Stack, Paper, Box, List, Grid, Typography } from "@mui/material";
-
 import PropertyInfo from "../../models/PropertyInfo";
-
 import RentalInfo from "../../models/RentalInfo";
-
-import dummyImage from "../../assets/images/flatties-icon-logo.png";
-
-import BathroomIcon from "@mui/icons-material/Bathroom";
-
-import BedIcon from "@mui/icons-material/Bed";
-
 import DummyData from "./dummyData.json";
-
 import ListingCard from "./ListingCard";
 import "./ListingPage.css";
 
