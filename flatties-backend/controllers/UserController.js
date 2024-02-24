c1df225179475b40e04078f0839a6ea26f4cd542
Shuai Wang <wsking233@gmail.com>
2024-02-24 22:20:39 +1300

    fix wrong commit error

--- flatties-backend/controllers/UserController.js
+++ flatties-backend/controllers/UserController.js
@@ -1,64 +1,136 @@
-const Users = require('../models/users');
+const UserModel = require('../models/UserModel');
 
-//get users
-const getUsers = (req, res) => {
-    Users.find({}, (err, users) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.send(users);
+const UserController = {
+    //get all users
+    getAllUser: async (req, res) => {
+        try {
+            const users = await UserModel.find();
+            res.status(200).json({users});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
         }
-    });
-};
-
-//get user by id
-const getUserById = (req, res) => {
-    Users.findById(req.params.id, (err, user) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.send(user);
+    },
+    //get user by id
+    getUserById: async (req, res) => {
+        try {
+            const user = await UserModel.findOne({_id: req.params._id});
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+            res.status(200).json({user});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
         }
-    });
-};
+    },
 
-//add user
-const addUser = (req, res) => {
-    Users.create(req.body, (err, user) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.send(user);
+    //create a new user
+    createUser : async (req, res) => {
+        console.log("createUser api called");
+        console.log(req.body);
+        try {
+            const user = await userModels.create(req.body);
+            //create a new watching list for the user
+            const watchingList = await watchingListModel.create({userId: user._id});
+            user.watchingList = watchingList._id;
+            const userWithWatchingList = await user.save();
+            res.json({user: userWithWatchingList, watchingList});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
         }
-    });
-};
+    },
 
-//update user
-const updateUser = (req, res) => {
-    Users.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.send(user);
+    //update user by id
+    updateUserById: async (req, res) => {
+        try {
+            const user = await UserModel
+                .findByIdAndUpdate({_id: req.params._id},
+                    req.body,
+                    {new: true});
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+
+            }
+            res.status(200).json({success: true, user});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
         }
-    });
-};
+    },
 
-//delete user
-const deleteUser = (req, res) => {
-    Users.findByIdAndDelete(req.params.id, (err, user) => {
-        if(err){
-            console.log(err);
-        }else{
-            res.send(user);
+    //delete user by id
+    deleteUserById: async (req, res) => {
+        try {
+            const user = await UserModel.findOneAndDelete({_id: req.params._id});
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+            res.status(200).json({success: true, user});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
         }
-    });
-};
+    },
 
-module.exports = {
-    getUsers,
-    getUserById,
-    addUser,
-    updateUser,
-    deleteUser
-};
+    //active user by id
+    activateUserById: async (req, res) => {
+        try {
+            const user = await UserModel
+                .findByIdAndUpdate({_id: req.params._id},
+                    {isActive: true},
+                    {new: true});
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+            res.status(200).json({success: true, user});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
+        }
+    },
+    //inactive user by id
+    inactiveUserById: async (req, res) => {
+        try {
+            const user = await UserModel
+                .findByIdAndUpdate({_id: req.params._id},
+                    {isActive: false},
+                    {new: true});
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+            res.status(200).json({success: true, user});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({message: "Server Error"});
+        }
+    },
+    //login
+    login : async (req, res) => {
+        console.log("login api called");
+        console.log(req.body);
+        try {
+            const user = await UserModel.findOne({email: req.body.email});
+
+            if (!user) {
+                return res.status(404).json({message: "User not found"});
+            }
+
+            if (req.body.password !== user.password) {
+                console.log("Password is incorrect");
+                return res.status(400).json({message: "Password is incorrect"});
+            }
+
+            res.status(200).json({message: "Login successful"});
+            console.log("Login successful");
+
+        } catch (error) {
+            console.log(error + " at login controller");
+            res.status(500).json({message: "Server Error"});
+        }
+    }
+
+}
+
+module.exports = UserController;
