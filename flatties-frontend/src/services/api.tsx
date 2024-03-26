62fa4c2bfb2438cbd614c3d0918f6cc353c53c48
Benjamin <144312142+Bnjmn-P@users.noreply.github.com>
2024-03-26 19:12:25 +1300

    fixed js-cookie

--- flatties-frontend/src/services/api.tsx
+++ flatties-frontend/src/services/api.tsx
@@ -1,15 +1,18 @@
-import axios, {AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 import CookieManager from './cookies/cookieManager';
 import config from '../config';
 
+const cookieManager = CookieManager();
+cookieManager.setCookie(" ", " "); 
+
+const baseURL = config.baseURL;
+const token = cookieManager.getCookie('token');
 
 // Create an axios instance
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: `${baseURL}`,
     timeout: 1000,
-    headers: {'Content-Type': 'application/json'}
+    headers: { 'Content-Type': 'application/json' }
 });
 
 export default instance;
-
-
