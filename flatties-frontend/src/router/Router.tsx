a80130062ea015ce9a7f67706d62fc3e53baf83c
Eirik Enriquez <eirikdbbd@gmail.com>
2024-04-04 16:27:26 +1300

    update router

--- flatties-frontend/src/router/Router.tsx
+++ flatties-frontend/src/router/Router.tsx
@@ -8,6 +8,8 @@ import RegisterPage from "../pages/register/RegisterPage";
 import NewPropertyForm from "../pages/newProperty/NewPropertyForm";
 import ActiveProperty from "../pages/newProperty/ActiveProperty";
 import ListingDetail from "../pages/listing/ListingDetail";
+import About from "../pages/about/AboutPage";
+import AboutPage from "../pages/about/AboutPage";
 
 function Router() {
   const handlePropertySubmit = (propertyData: any) => {
@@ -27,6 +29,7 @@ function Router() {
           <Route path="/new-property" element={<NewPropertyForm />} />
           <Route path="/active-property" element={<ActiveProperty />} />
           <Route path="/listing/:id" element={<ListingDetail />} />
+          <Route path="/about" element={<AboutPage />} />
 
           {/* 404 Page */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
