3fe9834e929013ea8b72b0318a02e55de0728a3b
Shuai Wang <wsking233@gmail.com>
2024-04-14 21:30:31 +1200

    Update Router.tsx
    add token check before navigate to some specific pages

--- flatties-frontend/src/router/Router.tsx
+++ flatties-frontend/src/router/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import HomePage from "../pages/home/HomePage";
 import ProfilePage from "../pages/profile/ProfilePage";
@@ -8,14 +8,20 @@ import RegisterPage from "../pages/register/RegisterPage";
 import NewPropertyForm from "../pages/newProperty/NewPropertyForm";
 import ActiveProperty from "../pages/newProperty/ActiveProperty";
 import ListingDetail from "../pages/listing/ListingDetail";
-import About from "../pages/about/AboutPage";
 import AboutPage from "../pages/about/AboutPage";
+import { useCookie } from "../services/cookies/CookieContext";
 
 function Router() {
-  const handlePropertySubmit = (propertyData: any) => {
-    // 处理新 property 提交逻辑
-    console.log("Property submitted:", propertyData);
-  };
+
+  const {getCookie} = useCookie();
+  let token = getCookie("token");
+
+  const PrivateRoute = ({element}: any) => {
+    //update token every time the route is accessed
+    token = getCookie("token");
+    //return the element if the token exists, otherwise redirect to the home page
+    return token ? element : <Navigate to="/" />;
+  }
 
   return (
     <BrowserRouter>
@@ -23,12 +29,12 @@ function Router() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/profile" element={<PrivateRoute element={<ProfilePage />} />} />
           <Route path="/listing" element={<ListingPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/new-property" element={<NewPropertyForm />} />
-          <Route path="/active-property" element={<ActiveProperty />} />
-          <Route path="/listing/:id" element={<ListingDetail />} />
+          <Route path="/new-property" element={<PrivateRoute element={<NewPropertyForm />} />} />
+          <Route path="/active-property" element={<PrivateRoute element={<ActiveProperty />} />} />
+          <Route path="/listing/:id" element={<PrivateRoute element={<ListingDetail />} />} />
           <Route path="/about" element={<AboutPage />} />
 
           {/* 404 Page */}
