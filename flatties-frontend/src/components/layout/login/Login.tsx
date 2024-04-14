710e13b098bdb1bb2a2855d163fc1069cb8c91a8
Shuai Wang <wsking233@gmail.com>
2024-04-14 21:23:22 +1200

    Update Login.tsx

--- flatties-frontend/src/components/layout/login/Login.tsx
+++ flatties-frontend/src/components/layout/login/Login.tsx
@@ -1,117 +1,116 @@
 import React from "react";
-import { Modal, Box, Typography, TextField, Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import {Modal, Box, Typography, TextField, Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 import axios from "../../../services/api";
-import useCookieManager from "../../../services/cookies/CookieManager";
+import {useCookie} from "../../../services/cookies/CookieContext";
 
 interface LoginModalProps {
-  open: boolean;
-  handleClose: () => void;
+    open: boolean;
+    handleClose: () => void;
 }
 
-function Login({ open, handleClose }: Readonly<LoginModalProps>) {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const navigate = useNavigate();
-  const { setCookie, getCookie, removeCookie } = useCookieManager();
+function Login({open, handleClose}: Readonly<LoginModalProps>) {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const navigate = useNavigate();
+    const {setCookie, getCookie, logout} = useCookie();
 
-  const handleLogin = async () => {
-    await axios
-      .post("/user/login", {
-        email: email,
-        password: password,
-      })
-      .then((res) => {
-        console.log("Login successful:", res.data);
+    const handleLogin = async () => {
+        try {
+            await axios.post('user/login', {
+                email: email,
+                password: password
+            }).then((response) => {
+                if (response.status === 200) {
+                    // Set the token cookie
+                    const token = response.data.token;
+                    const user = response.data.data;
+                    // Set the token cookie
+                    setCookie("token", token);
+                    setCookie("user", user);
+                    handleClose();
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-        // Set the token cookie to track the login status
-        setCookie('token', res.data.token);
-        setCookie('userName', res.data.userName);
-
-console.log(getCookie("userName"));
-        // Close the modal
+    const handleLogout = () => {
+        // Remove the token cookie
+        logout();
         handleClose();
-      })
-      .catch((err) => {
-        console.log("Login failed:", err);
-      });
-  };
+    };
 
-  const handleLogout = () => {
-    // Remove the token cookie
-    removeCookie("token");
-    handleClose();
-  };
+    const handleRegisterClick = () => {
+        // Navigate to "/register" and close the modal
+        navigate("/register");
+        handleClose(); // Close the modal without passing any arguments
+    };
 
-  const handleRegisterClick = () => {
-    // Navigate to "/register" and close the modal
-    navigate("/register");
-    handleClose(); // Close the modal without passing any arguments
-  };
+    // Check if token exists to determine login status
+    const token = getCookie("token");
 
-  // Check if token exists to determine login status
-  const token = getCookie("token");
-
-  return (
-    <Modal open={open} onClose={handleClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: 400,
-          bgcolor: "background.paper",
-          boxShadow: 24,
-          p: 4,
-        }}
-      >
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {token ? "Would you like to sign out?" : "Login"}
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {token ? (
-            <Button variant="contained" onClick={handleLogout} sx={{ mr: 2 }}>
-              Log out
-            </Button>
-          ) : (
-            <>
-              <TextField
-                fullWidth
-                label="Email"
-                variant="outlined"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                sx={{ mb: 2 }}
-              />
-              <TextField
-                fullWidth
-                label="Password"
-                type="password"
-                variant="outlined"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                sx={{ mb: 2 }}
-              />
-              <Button variant="contained" onClick={handleLogin} sx={{ mr: 2 }}>
-                Log In
-              </Button>
-              <Button
-                variant="contained"
-                onClick={handleRegisterClick}
-                sx={{ mr: 2 }}
-              >
-                Register
-              </Button>
-            </>
-          )}
-        </Typography>
-        <Button onClick={handleClose} sx={{ mt: 2 }}>
-          Close
-        </Button>
-      </Box>
-    </Modal>
-  );
+    return (
+        <Modal open={open} onClose={handleClose}>
+            <Box
+                sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    width: 400,
+                    bgcolor: "background.paper",
+                    boxShadow: 24,
+                    p: 4,
+                }}
+            >
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    {token ? "Would you like to sign out?" : "Login"}
+                </Typography>
+                <Typography id="modal-modal-description" sx={{mt: 2}}>
+                    {token ? (
+                        <Button variant="contained" onClick={handleLogout} sx={{mr: 2}}>
+                            Log out
+                        </Button>
+                    ) : (
+                        <>
+                            <TextField
+                                fullWidth
+                                label="Email"
+                                variant="outlined"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                sx={{mb: 2}}
+                            />
+                            <TextField
+                                fullWidth
+                                label="Password"
+                                type="password"
+                                variant="outlined"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                sx={{mb: 2}}
+                            />
+                            <Button variant="contained" onClick={handleLogin} sx={{mr: 2}}>
+                                Log In
+                            </Button>
+                            <Button
+                                variant="contained"
+                                onClick={handleRegisterClick}
+                                sx={{mr: 2}}
+                            >
+                                Register
+                            </Button>
+                        </>
+                    )}
+                </Typography>
+                <Button onClick={handleClose} sx={{mt: 2}}>
+                    Close
+                </Button>
+            </Box>
+        </Modal>
+    );
 }
 
 export default Login;
